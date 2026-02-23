a41443e7207d318657bcd17fa0d430f18664917e
kokc80 <kokc@inbox.ru>
2026-02-23 11:54:52 +0300

    проба работы на стороннем компе

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from catalog.apps import  CatalogConfig
+from catalog.views import catalog
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path("",catalog, name="home"
+                       "")
+]
