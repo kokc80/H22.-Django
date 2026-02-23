f1f02583c9dbc523f33152c09e578a923cea7eef
Kokc80 <kokc@inbox.ru>
2026-02-23 19:46:13 +0900

    не совсем понятно 20260223 19:45

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def catalog(request):
     return render(request, "home.html")
+
+
+def catalog_con(request):
+    return render(request, "contacts.html")
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls", namespace="catalog"))
+    path("", include("catalog.urls", namespace="catalog")),
+    path("contacts", include("catalog.urls", namespace="catalog_con"))
 ]
