1cb1661207ff297b76242c61f1ff965def14613a
Kokc80 <kokc@inbox.ru>
2026-02-25 21:18:31 +0900

    20260225 домашняя 23 создана БД, таблицы и .env изменен settincs.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+
 """
 Django settings for config project.
 
@@ -74,10 +76,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Загружаем переменные окружения из файла .env
+load_dotenv()
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
 
